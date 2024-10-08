2e5fc1cdd195b114ccb9409bb895e271a1d2483f
ang-len-26 <renelengua26@gmail.com>
2024-10-07 22:23:23 -0500

    commit inicial

---
+++ TUTORIAL/jsSample.js
@@ -0,0 +1,3 @@
+console.log("-----------");
+console.log("rise");
+console.log(".........");
---
+++ TUTORIAL/pythonSample.py
@@ -0,0 +1,4 @@
+print("-------------------------")
+print("Pythin has entered the building.")
+print("let the party start!")
+print("-------------------------")
